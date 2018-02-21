295851d4a5011e60b804c3cad250924cadc17d66
GodGetsGreens <r.wells6894@gmail.com>
2018-02-20 22:51:50 -0500

    Created new Registration form and source file. Have yet to tie the two forms together, next step will be to set up and create MySQL database and schemas, and to start adding user information to database.

--- src/forms/Login.java
+++ src/forms/Login.java
@@ -1,8 +1,8 @@
 package forms;
 
-import com.sun.codemodel.internal.JOp;
-
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.*;
@@ -14,7 +14,7 @@ public class Login {
     private JLabel lbUsername;
     private JLabel lbPassword;
     private JButton loginButton;
-    private JButton register;
+    private JButton create_user;
 
     public Login() {
         loginButton.addMouseListener(new MouseAdapter() {
@@ -26,23 +26,28 @@ public class Login {
                     JOptionPane.showMessageDialog(null, "Please enter password");
                 else {
                     try{
-                        BufferedReader fread = new BufferedReader(new FileReader(new File("forms/users.txt")));
+                        BufferedReader fread = new BufferedReader(new FileReader(new File("/Users/godgetsgreens/Documents/Database Design/Projects/Project4/DB-PROJ/src/forms/users.txt")));
                         String delim = "[,]";
                         String userInfo = fread.readLine();
+                        boolean exists = false;
                         while(userInfo != null){
                             String[] user = userInfo.split(delim);
                             if(userField.getText().equals(user[0]) && passField.getText().equals(user[1])){
                                 JOptionPane.showMessageDialog(null, "Login Successful!");
+                                exists = true;
                                 break;
                             }
                             else if(userField.getText().equals(user[0]) && !passField.getText().equals(user[1])){
                                 JOptionPane.showMessageDialog(null, "Invalid password!");
+                                exists = true;
                                 break;
                             }
                             else
                                 userInfo = fread.readLine();
                         }
                         fread.close();
+                        if(!exists)
+                            JOptionPane.showMessageDialog(null, "Username does not exist!");
                     }
                     catch(FileNotFoundException e1){
                         System.out.println(e1);
@@ -53,7 +58,7 @@ public class Login {
                 }
             }
         });
-        register.addMouseListener(new MouseAdapter() {
+        create_user.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
                 if(userField.getText().equals(""))
@@ -62,26 +67,25 @@ public class Login {
                     JOptionPane.showMessageDialog(null, "Please enter password");
                 else {
                     try {
-                        BufferedReader fread = new BufferedReader(new FileReader(new File("forms/users.txt")));
+                        BufferedReader fread = new BufferedReader(new FileReader(new File("/Users/godgetsgreens/Documents/Database Design/Projects/Project4/DB-PROJ/src/forms/users.txt")));
                         String delim = "[,]";
                         boolean exists = false;
                         String userInfo = fread.readLine();
-                        while(userInfo != null){
+                        while(userInfo != null && !exists){
                             String[] user = userInfo.split(delim);
                             if(userField.getText().equals(user[0])){
                                 JOptionPane.showMessageDialog(null, "Username Taken!");
                                 exists = true;
-                                break;
                             }
+                            userInfo = fread.readLine();
                         }
                         fread.close();
-                        if(!exists){
-                            BufferedWriter fwrite = new BufferedWriter(new FileWriter(new File("forms/users.txt")));
+                        if(!exists) {
+                            BufferedWriter fwrite = new BufferedWriter(new FileWriter(new File("/Users/godgetsgreens/Documents/Database Design/Projects/Project4/DB-PROJ/src/forms/users.txt")));
                             fwrite.write(userField.getText() + "," + passField.getText());
                             fwrite.newLine();
                             fwrite.close();
                         }
-
                     }
                     catch(FileNotFoundException e1){
                         System.out.println(e1);
@@ -94,6 +98,7 @@ public class Login {
 
             }
         });
+
     }
 
     public static void main(String[] args) {
---
+++ src/forms/Register.java
@@ -0,0 +1,28 @@
+package forms;
+
+import javax.swing.*;
+
+public class Register extends Login {
+    private JPanel registerPanel;
+    private JTextField new_fname;
+    private JTextField new_lname;
+    private JTextField new_username;
+    private JTextField new_email;
+    private JTextField new_pass;
+    private JLabel f_name;
+    private JLabel l_name;
+    private JLabel u_name;
+    private JTextField new_pass_conf;
+    private JLabel u_pass;
+    private JLabel confirm_pass;
+    private JLabel account;
+    private JTextField new_account;
+    private JButton user_register;
+
+    public Register() {
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
